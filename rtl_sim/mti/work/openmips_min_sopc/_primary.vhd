adcd845bcf79e1f02d9948947766d1fb1808851b
dtwang <415376530@qq.com>
2017-01-20 21:45:41 +0800

    Deleted some intermediate files.

--- rtl_sim/mti/work/openmips_min_sopc/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity openmips_min_sopc is
-    port(
-        clk             : in     vl_logic;
-        rst             : in     vl_logic
-    );
-end openmips_min_sopc;
--- rtl_sim/mti/work/openmips_min_sopc_tb/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity openmips_min_sopc_tb is
-end openmips_min_sopc_tb;
